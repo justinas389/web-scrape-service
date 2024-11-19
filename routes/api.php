bb08afbf219c08ab555836d70a5452671de2103c
Justinas Bernatavičius <bernatavicius.justinas@gmail.com>
2024-11-19 22:02:35 +0200

    Clean up default starter kit files

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\JobController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware(['auth:sanctum'])
     ->group(function () {
         Route::resource('jobs', JobController::class)->except('index', 'create', 'update', 'edit');
